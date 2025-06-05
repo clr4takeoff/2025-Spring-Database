d2b5547f73c86c0a54009e3a2b5f81c50efd5a67
clr4takeoff <jetblue22@o.cnu.ac.kr>
2025-06-05 17:23:18 +0900

    Add flght check UI

--- TP/temp.py
+++ TP/temp.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('flight_search.html')
+
+@app.route('/flight_search')
+def flight_search():
+    return render_template('flight_search.html')
+
+@app.route('/flight_check')
+def flight_check():
+    return render_template('flight_check.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
